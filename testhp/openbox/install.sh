7b6cbdc582b66032faba3907afcd2d80e3c41147
Nedyalko Dimitrov <nedyalko.dimitrov@technolink.org>
2021-06-10 00:09:29 +0300

    install parsec

--- testhp/openbox/install.sh
+++ testhp/openbox/install.sh
@@ -20,6 +20,11 @@ apt-get install -y conky
 # Install Node (required for pipe menu)
 apt-get install -y nodejs
 
+# Install Parsec client
+apt-get update
+apt-get install -y snapd
+snap install parsec --classic
+
 # Install Workspace Client
 apt-get install -y gnupg
 wget -q -O - https://workspaces-client-linux-public-key.s3-us-west-2.amazonaws.com/ADB332E7.asc | sudo apt-key add -
