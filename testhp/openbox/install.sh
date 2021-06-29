5430c157be71bfcfa8d90b8ebab6ce8141bf4a6e
Nedyalko Dimitrov <nedyalko.dimitrov@technolink.org>
2021-06-29 14:48:39 +0300

    parsec update

--- testhp/openbox/install.sh
+++ testhp/openbox/install.sh
@@ -31,6 +31,7 @@ chmod +x /etc/rc.local
 apt-get update
 apt-get install -y libqt5x11extras5
 wget https://builds.parsecgaming.com/package/parsec-linux.deb -O /home/zero
+chown zero:zero /home/zero/parsec-linux.deb
 dpkg -i parsec-linux.deb
 
 # Install Chromium-Browser
