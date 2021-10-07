ed91886efa73ad87ef550bd1c6ec8ae3b0130736
Teodor Linkin <teodor.linkin@technolink.org>
2021-10-07 16:43:49 +0300

    arrgggg

--- ansible/install.sh
+++ ansible/install.sh
@@ -5,7 +5,7 @@
 # ----------------------------------------------------------------------------------------------------------------------
 apt-get install -y ansible
 wget https://raw.githubusercontent.com/techno-link/zero-client/master/ansible/zero.yml -O /root/zero.yml
-ansible-playbook /root/zero.yaml -e skip_handlers=true
+ansible-playbook /root/zero.yml -e skip_handlers=true
 
 # ----------------------------------------------------------------------------------------------------------------------
 # AUTOSTART OPENBOX CONFIG
