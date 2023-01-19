456cd420bfc6535c548230fe3f34dfa98821dadd
aleks-ander <120628511+aleks-ander@users.noreply.github.com>
2023-01-19 16:55:49 +0200

    Update ansible-cron.sh

--- ansible/ansible-cron.sh
+++ ansible/ansible-cron.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget -q https://raw.githubusercontent.com/aleks-ander/zero-client-update-to-fluxbox-20.04/test/ansible/zero.yml -O /root/zero.yml
-wget -q https://raw.githubusercontent.com/aleks-ander/zero-client-update-to-fluxbox-20.04/test/ansible/run-once.yml -O /root/run-once.yml
+wget -q https://raw.githubusercontent.com/techno-link/zero-client/master/ansible/zero.yml -O /root/zero.yml
+wget -q https://raw.githubusercontent.com/techno-link/zero-client/master/ansible/run-once.yml -O /root/run-once.yml
 ANSIBLE_LOG_PATH=/root/ansible.log ansible-playbook /root/zero.yml -v
 ANSIBLE_LOG_PATH=/root/ansible_once.log ansible-playbook /root/run-once.yml -v
