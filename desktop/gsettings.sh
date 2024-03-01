c4f2a563333e351eb101beaed17537c10af192bb
manna <teodor.linkin@technolink.org>
2024-03-01 16:12:54 +0200

    fix(ansible): Fix Gnome Minimal installation and configuration
    - Install Gnome Minimal instead of LightDM
    - Configure GDM for auto-login
    - Set preferred session for auto-login user
    - Ensure GDM is enabled and running
    - Update wallpaper and screensaver settings
    - Set virtual workspaces to false
    - Set GTK theme to Yaru-dark
    
    Issue: v3

--- desktop/gsettings.sh
+++ desktop/gsettings.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+# KEYBOARD
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'bg'), ('xkb', 'bg+phonetic')]"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Super>space']"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Shift><Super>space']"
-gsettings set org.gnome.desktop.background picture-uri '/home/zero/.config/linkin/wallpaper.jpg'
+
+# WALLPAPER
+gsettings set org.gnome.desktop.background picture-uri 'file:///home/zero/.config/linkin/wallpaper.jpg'
+gsettings set org.gnome.desktop.background picture-uri-dark 'file:///home/zero/.config/linkin/wallpaper.jpg'
+gsettings set org.gnome.desktop.background primary-color #000000000000
+gsettings set org.gnome.desktop.background secondary-color #000000000000
+
+gsettings set org.gnome.desktop.screensaver picture-uri 'file:///home/zero/.config/linkin/wallpaper.jpg'
+gsettings set org.gnome.desktop.screensaver primary-color #000000000000
+gsettings set org.gnome.desktop.screensaver secondary-color #000000000000
+
+# VIRTUAL WORKSPACES (MULTITASKING)
+gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 1
+
+# THEME DARK
+gsettings set org.gnome.desktop gtk-theme 'Yaru-dark'
+
