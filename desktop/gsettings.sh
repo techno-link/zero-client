2caecf4b73a0dd7fd73dd8b0418e7e5cdd42f113
manna <teodor.linkin@technolink.org>
2024-03-01 18:58:00 +0200

    fix(image-creation): fix useradd command in modify-chroot.sh
    The useradd command was missing a flag, causing the user creation to fail. Adding the missing flag ensures the user is created successfully.

--- desktop/gsettings.sh
+++ desktop/gsettings.sh
@@ -29,6 +29,6 @@ gsettings set org.gnome.shell favorite-apps ['com.amazon.workspacesclient.deskto
 gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'interactive'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 900
 
-# LOCK SCREEN
-gsettings set org.gnome.desktop.screensaver lock-enabled true
+## LOCK SCREEN
+#gsettings set org.gnome.desktop.screensaver lock-enabled true
 gsettings set org.gnome.desktop.lockdown disable-lock-screen false
