a83a8bdad8616e13a4587a31d0bbe29a0cd97409
manna <teodor.linkin@technolink.org>
2024-03-01 16:33:30 +0200

    refactor(api): remove obsolete todo in ansible/zero.yml and modify-chroot.sh
    Remove obsolete todo in ansible/zero.yml and update default user creation in
    modify-chroot.sh to include a comment for clarity. These changes enhance code
    readability and maintainability.

--- desktop/gsettings.sh
+++ desktop/gsettings.sh
@@ -8,12 +8,12 @@ gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<
 # WALLPAPER
 gsettings set org.gnome.desktop.background picture-uri 'file:///home/zero/.config/linkin/wallpaper.jpg'
 gsettings set org.gnome.desktop.background picture-uri-dark 'file:///home/zero/.config/linkin/wallpaper.jpg'
-gsettings set org.gnome.desktop.background primary-color #000000000000
-gsettings set org.gnome.desktop.background secondary-color #000000000000
+gsettings set org.gnome.desktop.background primary-color '#000000000000'
+gsettings set org.gnome.desktop.background secondary-color '#000000000000'
 
 gsettings set org.gnome.desktop.screensaver picture-uri 'file:///home/zero/.config/linkin/wallpaper.jpg'
-gsettings set org.gnome.desktop.screensaver primary-color #000000000000
-gsettings set org.gnome.desktop.screensaver secondary-color #000000000000
+gsettings set org.gnome.desktop.screensaver primary-color '#000000000000'
+gsettings set org.gnome.desktop.screensaver secondary-color '#000000000000'
 
 # VIRTUAL WORKSPACES (MULTITASKING)
 gsettings set org.gnome.mutter dynamic-workspaces false
@@ -22,3 +22,9 @@ gsettings set org.gnome.desktop.wm.preferences num-workspaces 1
 # THEME DARK
 gsettings set org.gnome.desktop gtk-theme 'Yaru-dark'
 
+# FAV APPS
+gsettings set org.gnome.shell favorite-apps ['com.amazon.workspacesclient.desktop', 'parsecd.desktop', 'gnome-system-monitor.desktop']
+
+# POWER SETTINGS
+gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'interactive'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 900
