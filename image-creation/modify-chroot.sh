87ad6ade7a4882e774510ae1af28ef40cfae1f03
manna <teodor.linkin@technolink.org>
2025-03-21 20:37:48 +0200

    ```markdown feat(ansible): Update log path and modify sudo access for user "zero" (v3)
    Update ANSIBLE_LOG_PATH in service files to point to the "zero" directory.
    Comment out passwordless sudo access in modify-chroot.sh for "zero". This
    change aims to centralize log paths and manage sudo permissions more
    effectively.
    ```

--- image-creation/modify-chroot.sh
+++ image-creation/modify-chroot.sh
@@ -17,8 +17,8 @@ systemctl enable ansible-first-boot.service
 
 # ADD DEFAULT USER
 useradd -m -c "Linkin Zero Client" -d /home/zero -s /bin/bash zero
-echo "zero ALL=(ALL) NOPASSWD: ALL" > "/etc/sudoers.d/zero"
-chmod 440 "/etc/sudoers.d/zero"
+#echo "zero ALL=(ALL) NOPASSWD: ALL" > "/etc/sudoers.d/zero"
+#chmod 440 "/etc/sudoers.d/zero"
 
 # RUN ANSIBLE INSIDE CHROOT
 ZEROSTATE=CHROOT ansible-playbook /root/zero.yml -v
